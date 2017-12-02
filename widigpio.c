65b2108fc6817179c267c1877d26c7af3d86d999
Dirk Winkhaus <dirk@googlemail.com>
2017-12-02 06:44:41 +0100

    fix input read function

--- widigpio.c
+++ widigpio.c
@@ -29,30 +29,30 @@ PHP_METHOD(Gpio, setExport)
     char file_name[24];
 
     ZEND_PARSE_PARAMETERS_START(2, 2)
-      Z_PARAM_LONG(port)
-      Z_PARAM_BOOL(export)
+        Z_PARAM_LONG(port)
+        Z_PARAM_BOOL(export)
     ZEND_PARSE_PARAMETERS_END();
-	
-	if (export) {
-		strcpy(file_name, "/sys/class/gpio/export");
-	} else {
-		strcpy(file_name, "/sys/class/gpio/unexport");
-	}
-	
-	fd = open(file_name, O_WRONLY);
-	if (-1 == fd) {
-    	RETURN_FALSE;
-	}
+    
+    if (export) {
+        strcpy(file_name, "/sys/class/gpio/export");
+    } else {
+        strcpy(file_name, "/sys/class/gpio/unexport");
+    }
+    
+    fd = open(file_name, O_WRONLY);
+    if (-1 == fd) {
+        RETURN_FALSE;
+    }
  
-	bytes_written = snprintf(buffer, 3, "%pd", port);
-	
-	if (-1 == write(fd, buffer, bytes_written)) {
-		RETURN_FALSE;	
-	}
-	
-	close(fd);
+    bytes_written = snprintf(buffer, 3, "%pd", port);
+    
+    if (-1 == write(fd, buffer, bytes_written)) {
+        RETURN_FALSE;    
+    }
+    
+    close(fd);
 
-	RETURN_TRUE;
+    RETURN_TRUE;
 }
 /* }}} */
 
@@ -60,41 +60,37 @@ PHP_METHOD(Gpio, setExport)
  */
 PHP_METHOD(Gpio, setDirection)
 {
-	zend_long port;
-	zend_bool direction;
-	char direction_str[3];
- 	char path[35];
-	int fd;
+    zend_long port;
+    zend_bool direction;
+    char direction_str[3];
+     char path[35];
+    int fd;
 
     ZEND_PARSE_PARAMETERS_START(2, 2)
       Z_PARAM_LONG(port)
       Z_PARAM_BOOL(direction)
     ZEND_PARSE_PARAMETERS_END();
-	
-	snprintf(path, 35, "/sys/class/gpio/gpio%pd/direction", port);
-	printf("%s", path);
-	printf("\n");
-	
-	fd = open(path, O_WRONLY);
-	if (-1 == fd) {
-		RETURN_FALSE;
-	}
-	
-	if (direction) {
-		strcpy(direction_str, "out");
-	} else {
-		strcpy(direction_str, "in");
-	}
-	
-	printf("%s", direction_str);
+    
+    snprintf(path, 35, "/sys/class/gpio/gpio%pd/direction", port);
+    
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        RETURN_FALSE;
+    }
+    
+    if (direction) {
+        strcpy(direction_str, "out");
+    } else {
+        strcpy(direction_str, "in");
+    }
+    
+    if (-1 == write(fd, direction_str, strlen(direction_str))) {
+        RETURN_FALSE;
+    }
  
-	if (-1 == write(fd, direction_str, strlen(direction_str))) {
-		RETURN_FALSE;
-	}
- 
-	close(fd);
+    close(fd);
 
-	RETURN_TRUE;
+    RETURN_TRUE;
 }
 /* }}}*/
 
@@ -102,37 +98,37 @@ PHP_METHOD(Gpio, setDirection)
  */
 PHP_METHOD(Gpio, setIo)
 {
-	zend_long port;
-	zend_bool set;
-	char path[35];
-	char set_str[1];
-	int fd;
+    zend_long port;
+    zend_bool set;
+    char path[35];
+    char set_str[1];
+    int fd;
 
     ZEND_PARSE_PARAMETERS_START(2, 2)
       Z_PARAM_LONG(port)
       Z_PARAM_BOOL(set)
     ZEND_PARSE_PARAMETERS_END();
  
-	snprintf(path, 35, "/sys/class/gpio/gpio%pd/value", port);
-	
-	fd = open(path, O_WRONLY);
-	if (-1 == fd) {
-		RETURN_FALSE;
-	}
-	
-	if (set) {
-		strcpy(set_str, "1");
-	} else {
-		strcpy(set_str, "0");
-	}
+    snprintf(path, 35, "/sys/class/gpio/gpio%pd/value", port);
+    
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        RETURN_FALSE;
+    }
+    
+    if (set) {
+        strcpy(set_str, "1");
+    } else {
+        strcpy(set_str, "0");
+    }
  
-	if (1 != write(fd, set_str, 1)) {
-		RETURN_FALSE;
-	}
+    if (1 != write(fd, set_str, 1)) {
+        RETURN_FALSE;
+    }
  
-	close(fd);
+    close(fd);
 
-	RETURN_TRUE;
+    RETURN_TRUE;
 }
 /* }}}*/
 
@@ -140,33 +136,35 @@ PHP_METHOD(Gpio, setIo)
  */
 PHP_METHOD(Gpio, getIo)
 {
-	zend_long port;
-	char path[35];
-	char input[1];
-	int fd;
+    zend_long port;
+    char path[35];
+    char input[1];
+    int fd;
 
     ZEND_PARSE_PARAMETERS_START(1, 1)
       Z_PARAM_LONG(port)
     ZEND_PARSE_PARAMETERS_END();
  
-	snprintf(path, 35, "/sys/class/gpio/gpio%pd/value", port);
-	
-	fd = open(path, O_RDONLY);
-	if (-1 == fd) {
-		RETURN_NULL();
-	}
- 
-	if (-1 == read(fd, input, 1)) {
-		RETURN_NULL();
-	}
+    snprintf(path, 35, "/sys/class/gpio/gpio%pd/value", port);
+    
+    fd = open(path, O_RDONLY);
+    if (-1 == fd) {
+        RETURN_NULL();
+    }
+ 	
+ 	memset(input,0,strlen(input));
+    
+    if (-1 == read(fd, input, 1)) {
+        RETURN_NULL();
+    }
 
-	close(fd);
-	
-	if (strcmp(input, "1") == 0) {
-		RETURN_TRUE;
-	} else {
-		RETURN_FALSE;
-	}
+    close(fd);
+
+    if (strcmp(input, "1") == 0) {
+        RETURN_TRUE;
+    } else {
+        RETURN_FALSE;
+    }
 }
 /* }}}*/
 
@@ -176,29 +174,29 @@ PHP_METHOD(Gpio, getOneWireDeviceList)
 {
     char input_string[1024];
     char *part_string;
-	int fd;
+    int fd;
 
     ZEND_PARSE_PARAMETERS_START(0, 0)
     ZEND_PARSE_PARAMETERS_END();
-	
-	fd = open("/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves", O_RDONLY);
-	if (-1 == fd) {
-		RETURN_NULL();
-	}
+    
+    fd = open("/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves", O_RDONLY);
+    if (-1 == fd) {
+        RETURN_NULL();
+    }
  
-	if (-1 == read(fd, input_string, 1024)) {
-		RETURN_NULL();
-	}
+    if (-1 == read(fd, input_string, 1024)) {
+        RETURN_NULL();
+    }
 
-	close(fd);
+    close(fd);
 
-	array_init(return_value);		
+    array_init(return_value);        
     part_string = strtok(input_string, "\n");
     
     if (part_string != NULL) {
-		add_next_index_string(return_value, part_string);
+        add_next_index_string(return_value, part_string);
         while ((part_string = strtok(NULL, "\n")) != NULL) {
-			add_next_index_string(return_value, part_string);
+            add_next_index_string(return_value, part_string);
         }
     }
 }
@@ -208,36 +206,36 @@ PHP_METHOD(Gpio, getOneWireDeviceList)
  */
 PHP_METHOD(Gpio, getOneWireDeviceValue)
 {
-	char *device_name;
-	size_t device_name_len;
-	char path[64];
+    char *device_name;
+    size_t device_name_len;
+    char path[64];
     char input_string[1024];
-	char *temp_string;
-	long temp;
-	int fd;
+    char *temp_string;
+    long temp;
+    int fd;
 
     ZEND_PARSE_PARAMETERS_START(1, 1)
-		Z_PARAM_STRING(device_name, device_name_len)
+        Z_PARAM_STRING(device_name, device_name_len)
     ZEND_PARSE_PARAMETERS_END();
-	
-	snprintf(path, 64, "/sys/bus/w1/devices/%s/w1_slave", device_name);
-	
-	fd = open(path, O_RDONLY);
-	if (-1 == fd) {
-		RETURN_NULL();
-	}
+    
+    snprintf(path, 64, "/sys/bus/w1/devices/%s/w1_slave", device_name);
+    
+    fd = open(path, O_RDONLY);
+    if (-1 == fd) {
+        RETURN_NULL();
+    }
  
-	if (-1 == read(fd, input_string, 1024)) {
-		RETURN_NULL();
-	}
-	
-	close(fd);
+    if (-1 == read(fd, input_string, 1024)) {
+        RETURN_NULL();
+    }
+    
+    close(fd);
 
-	temp_string = strstr(input_string, "t=");
-	memmove(temp_string, temp_string+2, strlen(temp_string));
-	temp = atol(temp_string);
+    temp_string = strstr(input_string, "t=");
+    memmove(temp_string, temp_string+2, strlen(temp_string));
+    temp = atol(temp_string);
 
-	RETURN_LONG(temp);
+    RETURN_LONG(temp);
 }
 /* }}}*/
 
@@ -246,10 +244,10 @@ PHP_METHOD(Gpio, getOneWireDeviceValue)
 PHP_RINIT_FUNCTION(widigpio)
 {
 #if defined(ZTS) && defined(COMPILE_DL_WIDIGPIO)
-	ZEND_TSRMLS_CACHE_UPDATE();
+    ZEND_TSRMLS_CACHE_UPDATE();
 #endif
 
-	return SUCCESS;
+    return SUCCESS;
 }
 /* }}} */
 
@@ -257,51 +255,51 @@ PHP_RINIT_FUNCTION(widigpio)
  */
 PHP_MINFO_FUNCTION(widigpio)
 {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "widigpio support", "enabled");
-	php_info_print_table_end();
+    php_info_print_table_start();
+    php_info_print_table_header(2, "widigpio support", "enabled");
+    php_info_print_table_end();
 }
 /* }}} */
 
 /* {{{ arginfo
  */
 ZEND_BEGIN_ARG_INFO(arginfo_gpio_export, 0)
-	ZEND_ARG_INFO(0, int)
-	ZEND_ARG_INFO(0, bool)
+    ZEND_ARG_INFO(0, int)
+    ZEND_ARG_INFO(0, bool)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO(arginfo_gpio_direction, 0)
-	ZEND_ARG_INFO(0, int)
-	ZEND_ARG_INFO(0, bool)
+    ZEND_ARG_INFO(0, int)
+    ZEND_ARG_INFO(0, bool)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO(arginfo_gpio_set, 0)
-	ZEND_ARG_INFO(0, int)
-	ZEND_ARG_INFO(0, bool)
+    ZEND_ARG_INFO(0, int)
+    ZEND_ARG_INFO(0, bool)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO(arginfo_gpio_get, 0)
-	ZEND_ARG_INFO(0, int)
+    ZEND_ARG_INFO(0, int)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO(arginfo_gpio_onewire_slaves_list, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO(arginfo_gpio_onewire_slave_value, 0)
-	ZEND_ARG_INFO(0, char[])
+    ZEND_ARG_INFO(0, char[])
 ZEND_END_ARG_INFO()
 /* }}} */
 
 /* {{{ widigpio_functions[]
  */
 const zend_function_entry widigpio_functions[] = {
-	PHP_ME(Gpio, setExport,	arginfo_gpio_export, ZEND_ACC_PUBLIC)
-	PHP_ME(Gpio, setDirection, arginfo_gpio_direction, ZEND_ACC_PUBLIC)
-	PHP_ME(Gpio, setIo, arginfo_gpio_set, ZEND_ACC_PUBLIC)
-	PHP_ME(Gpio, getIo, arginfo_gpio_get, ZEND_ACC_PUBLIC)
-	PHP_ME(Gpio, getOneWireDeviceList, arginfo_gpio_onewire_slaves_list, ZEND_ACC_PUBLIC)
-	PHP_ME(Gpio, getOneWireDeviceValue, arginfo_gpio_onewire_slave_value, ZEND_ACC_PUBLIC)
-	PHP_FE_END
+    PHP_ME(Gpio, setExport,    arginfo_gpio_export, ZEND_ACC_PUBLIC)
+    PHP_ME(Gpio, setDirection, arginfo_gpio_direction, ZEND_ACC_PUBLIC)
+    PHP_ME(Gpio, setIo, arginfo_gpio_set, ZEND_ACC_PUBLIC)
+    PHP_ME(Gpio, getIo, arginfo_gpio_get, ZEND_ACC_PUBLIC)
+    PHP_ME(Gpio, getOneWireDeviceList, arginfo_gpio_onewire_slaves_list, ZEND_ACC_PUBLIC)
+    PHP_ME(Gpio, getOneWireDeviceValue, arginfo_gpio_onewire_slave_value, ZEND_ACC_PUBLIC)
+    PHP_FE_END
 };
 /* }}} */
 
@@ -318,16 +316,16 @@ PHP_MINIT_FUNCTION(widigpio)
 /* {{{ widigpio_module_entry
  */
 zend_module_entry widigpio_module_entry = {
-	STANDARD_MODULE_HEADER,
-	"widigpio",					/* Extension name */
-	widigpio_functions,			/* zend_function_entry */
-	PHP_MINIT(widigpio),		/* PHP_MINIT - Module initialization */
-	NULL,						/* PHP_MSHUTDOWN - Module shutdown */
-	PHP_RINIT(widigpio),		/* PHP_RINIT - Request initialization */
-	NULL,						/* PHP_RSHUTDOWN - Request shutdown */
-	PHP_MINFO(widigpio),		/* PHP_MINFO - Module info */
-	PHP_WIDIGPIO_VERSION,		/* Version */
-	STANDARD_MODULE_PROPERTIES
+    STANDARD_MODULE_HEADER,
+    "widigpio",                    /* Extension name */
+    widigpio_functions,            /* zend_function_entry */
+    PHP_MINIT(widigpio),        /* PHP_MINIT - Module initialization */
+    NULL,                        /* PHP_MSHUTDOWN - Module shutdown */
+    PHP_RINIT(widigpio),        /* PHP_RINIT - Request initialization */
+    NULL,                        /* PHP_RSHUTDOWN - Request shutdown */
+    PHP_MINFO(widigpio),        /* PHP_MINFO - Module info */
+    PHP_WIDIGPIO_VERSION,        /* Version */
+    STANDARD_MODULE_PROPERTIES
 };
 /* }}} */
 
